3036889d2c19a9b4b751f4e5a71624d59a1a0019
jds <jds@gritstandard.com>
2026-06-16 15:26:37 +0900

    chore: scaffold Spring Boot project (web, jpa, h2)

---
+++ src/main/java/com/turtlesoup/TurtlesoupApplication.java
@@ -0,0 +1,13 @@
+package com.turtlesoup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtlesoupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtlesoupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turtlesoup/TurtlesoupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtlesoup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtlesoupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
